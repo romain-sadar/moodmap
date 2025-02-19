dcc1cd1ba9174d0bc127675ddf6972e2b2661474
Alice MATHEY <145433804+Alice030303@users.noreply.github.com>
2025-02-19 12:00:16 +0800

    Création navLayout, bouton "list" quand on scroll le split screen en entier pour le réouvrir, navBar

--- moodmap/lib/main.dart
+++ moodmap/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:moodmap/core/themes.dart';
+import 'package:moodmap/views/nav_layout.dart';
 import 'views/home/home_screen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       
-     home: HomeScreen(),
+     home: NavLayout(),
     );
   }
 }
--- moodmap/lib/views/home/home_screen.dart
+++ moodmap/lib/views/home/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:moodmap/core/themes.dart';
 import 'package:moodmap/models/place_model.dart';
+import 'package:moodmap/views/home/widgets/navbar.dart';
 import 'widgets/listing_box.dart';
 import 'widgets/map_view.dart';
 
@@ -16,9 +17,20 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  bool isOnMap = true; // État pour savoir si on est sur la carte ou sur les activités
-  bool showButtons = false; // Contrôle la visibilité des boutons
-  double currentSheetSize = 0.3; // Taille actuelle du DraggableScrollableSheet
+  bool isOnMap = true;
+  bool showButtons = false;
+  bool showListButton = false;
+  double currentSheetSize = 0.4;
+  int selectedIndex = 0;
+
+  final DraggableScrollableController _sheetController =
+      DraggableScrollableController(); // Ajout du contrôleur
+
+  void onTabSelected(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,11 +51,10 @@ class _HomeScreenState extends State<HomeScreen> {
       tags: ['Café', 'Work'],
       type: '☕️',
     );
-
-    ListingItem activity1 = ListingItem(
+  ListingItem activity1 = ListingItem(
       imagePath: 'assets/images/brunchesCafe.jpg',
       title: 'Lecture',
-      distance: '2min',
+      distance: '3min',
       description: 'Cozy bruncher.',
       tags: ['Calm', 'Relax'],
       type: '☕️',
@@ -51,129 +62,155 @@ class _HomeScreenState extends State<HomeScreen> {
 
     ListingItem activity2 = ListingItem(
       imagePath: 'assets/images/brunchesCafe.jpg',
-      title: 'Top',
-      distance: '1min',
+      title: 'Coffee Shop',
+      distance: '1h',
       description: 'Perfect place for a coffee break.',
       tags: ['Café', 'Work'],
-      type: '☕️',
+      type: '📚',
     );
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SafeArea(
+        bottom: false,
         child: Scaffold(
           body: Stack(
             children: [
               MapScreen(),
               Column(
                 children: [
-                  // DraggableScrollableSheet
                   Expanded(
-                    child: NotificationListener<DraggableScrollableNotification>(
-                      onNotification: (notification) {
-                        // Mise à jour de la taille actuelle du sheet
-                       setState(() {
-                        currentSheetSize = notification.extent;
-                        
-                        // Si le sheet est à sa taille maximale, on affiche les boutons
-                        if (currentSheetSize == 1.0) {
-                          showButtons = true;
-                        } else {
-                          isOnMap = true;
-                          showButtons = false;
-                        }
-                      });
-                        return true;
-                      },
-                      child: DraggableScrollableSheet(
-                        initialChildSize: 0.4,
-                        minChildSize: 0.4,
-                        maxChildSize: 1.0,
-                        builder: (BuildContext context, scrollController) {
-                          return Container(
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20),
-                              ),
-                            ),
-                            child: Column(
-                              children: [
-                                // Affichage des boutons "Map" et "Activities" si showButtons est true
-                                if (showButtons)
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 10.0),
-                                    child: Row(
-                                      mainAxisAlignment: MainAxisAlignment.center,
-                                      children: [
-                                        ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: isOnMap ? AppTheme.blue : Colors.grey,
-                                            foregroundColor: Colors.white,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.only(
-                                                topLeft: Radius.circular(20),  
-                                                bottomLeft: Radius.circular(20), 
-                                                topRight: Radius.circular(0),  
-                                                bottomRight: Radius.circular(0), 
-                                              ),
-                                            ),
-                                          ),
-                                          onPressed: () {
-                                            setState(() {
-                                              isOnMap = true;
-                                            });
-                                          },
-                                          child: Icon(Icons.map, color: Colors.white),
-                                        ),
-                                        
-                                        ElevatedButton(
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor: !isOnMap ? AppTheme.violet : Colors.grey,
-                                            foregroundColor: Colors.white,
-                                            shape: RoundedRectangleBorder(
-                                              borderRadius: BorderRadius.only(
-                                                topLeft: Radius.circular(0),  
-                                                bottomLeft: Radius.circular(0), 
-                                                topRight: Radius.circular(20),  
-                                                bottomRight: Radius.circular(20), 
-                                              ),
-                                            ),
-                                          ),
-                                          onPressed: () {
-                                            setState(() {
-                                              isOnMap = false;
-                                            });
-                                          },
-                                          child: Icon(Icons.location_off, color: Colors.white),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                // Liste des éléments sur la carte ou les activités
-                                Expanded(
-                                  child: ListView(
-                                    controller: scrollController,
-                                    children: [
-                                      if (isOnMap) ...[
-                                        ListingBox(item: item1),
-                                        SizedBox(height:8),
-                                        ListingBox(item: item2),
-                                      ] else ...[
-                                        ListingBox(item: activity1),
-                                        SizedBox(height:8),
-                                        ListingBox(item: activity2),
-                                      ],
-                                    ],
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        NotificationListener<DraggableScrollableNotification>(
+                          onNotification: (notification) {
+                            setState(() {
+                              currentSheetSize = notification.extent;
+
+                              // Affichage du bouton si la liste est en bas
+                              showListButton = currentSheetSize <= 0.1;
+                              isOnMap= currentSheetSize<=1;
+                              // Affichage des boutons de navigation si la liste est complètement ouverte
+                              showButtons = currentSheetSize == 1.0;
+
+                            });
+                            return true;
+                          },
+                          child: DraggableScrollableSheet(
+                            controller: _sheetController, // Assignation du contrôleur
+                            initialChildSize: 0.4,
+                            minChildSize: 0,
+                            maxChildSize: 1.0,
+                            builder: (BuildContext context, scrollController) {
+                              return Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    topRight: Radius.circular(20),
                                   ),
                                 ),
-                              ],
+                                child: Column(
+                                  children: [
+                                    if (showButtons)
+                                      Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10.0),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                backgroundColor: isOnMap
+                                                    ? AppTheme.blue
+                                                    : Colors.grey,
+                                                foregroundColor: Colors.white,
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                    topLeft:
+                                                        Radius.circular(20),
+                                                    bottomLeft:
+                                                        Radius.circular(20),
+                                                  ),
+                                                ),
+                                              ),
+                                              onPressed: () {
+                                                setState(() {
+                                                  isOnMap = true;
+                                                });
+                                              },
+                                              child: Icon(Icons.map,
+                                                  color: Colors.white),
+                                            ),
+                                            ElevatedButton(
+                                              style: ElevatedButton.styleFrom(
+                                                backgroundColor: !isOnMap
+                                                    ? AppTheme.violet
+                                                    : Colors.grey,
+                                                foregroundColor: Colors.white,
+                                                shape: RoundedRectangleBorder(
+                                                  borderRadius:
+                                                      BorderRadius.only(
+                                                    topRight:
+                                                        Radius.circular(20),
+                                                    bottomRight:
+                                                        Radius.circular(20),
+                                                  ),
+                                                ),
+                                              ),
+                                              onPressed: () {
+                                                setState(() {
+                                                  isOnMap = false;
+                                                });
+                                              },
+                                              child: Icon(Icons.location_off,
+                                                  color: Colors.white),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    Expanded(
+                                      child: ListView(
+                                        controller: scrollController,
+                                        children: [
+                                          if (isOnMap) ...[
+                                            ListingBox(item: item1),
+                                            SizedBox(height: 8),
+                                            ListingBox(item: item2),
+                                          ] else ...[
+                                            ListingBox(item: activity1),
+                                            SizedBox(height: 8),
+                                            ListingBox(item: activity2),
+                                          ],
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                        if (showListButton)
+                          Positioned(
+                            bottom: 20,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(backgroundColor:  AppTheme.blue, ),
+                              onPressed: () {
+                                _sheetController.animateTo(
+                                  1.0, 
+                                  duration: Duration(milliseconds: 300),
+                                  curve: Curves.easeInOut,
+                                );
+                              },
+                              child: Row
+                              (children:[Icon(Icons.list, color: Colors.black,),SizedBox(width:10), Text("List",  style: TextStyle(color: Colors.black),) ]),
                             ),
-                          );
-                        },
-                      ),
+                          ),
+                      ],
                     ),
                   ),
                 ],
