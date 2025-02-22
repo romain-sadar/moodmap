b6c976fa83c17bb8aea1111f6ac460418b8533a3
Alice MATHEY <145433804+Alice030303@users.noreply.github.com>
2025-02-22 11:40:57 +0800

    -moodform1 -moodform2

--- moodmap/lib/views/home/widgets/mood_form_location.dart
+++ moodmap/lib/views/home/widgets/mood_form_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:duration_picker/duration_picker.dart';
 
 enum Mood {
   verySad("😢", "Very Sad"),
@@ -26,16 +27,16 @@ enum Mood {
     }
   }
 }
-class MoodFormScreen extends StatefulWidget {
+class MoodFormLocationScreen extends StatefulWidget {
   @override
-  _MoodFormScreenState createState() => _MoodFormScreenState();
+  _MoodFormLocationScreenState createState() => _MoodFormLocationScreenState();
 }
 
-class _MoodFormScreenState extends State<MoodFormScreen> {
-  Mood _moodValue = Mood.happy; // Valeur initiale du slider
+class _MoodFormLocationScreenState extends State<MoodFormLocationScreen> {
+  Mood _moodValue = Mood.happy; 
   List<String> selectedTags = [];
-  bool wantsToMove = false; // Pour la section "Do you want to move?"
-
+  Duration _selectedDuration = Duration(hours: 0, minutes: 0);
+  bool favorites=false;
   void toggleTag(String tag) {
     setState(() {
       if (selectedTags.contains(tag)) {
@@ -45,8 +46,71 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
       }
     });
   }
-
   
+
+   Future<void> _pickDuration() async {
+    Duration? picked = await showDurationPicker(
+      context: context,
+      initialTime: _selectedDuration,
+      baseUnit: BaseUnit.minute,
+    
+      decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(15),
+    ),
+      
+    );
+
+    if (picked != null) {
+      setState(() {
+        _selectedDuration = picked;
+      });
+    }
+  }
+
+  void _showFavoriteDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text("Add to Favorites?"),
+        backgroundColor: Colors.grey[200],
+        content: Text("Do you want to add this place to your favorites?"),
+        actions: [
+          TextButton(
+            onPressed: () {
+              setState(() {
+                favorites = false;
+              });
+              print("Favorite set to: $favorites");
+              Navigator.of(context).pop();
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.grey, 
+            ),
+            child: Text("No"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                favorites = true;
+              });
+              
+              print("Favorite set to: $favorites");
+              Navigator.of(context).pop(); 
+            },
+             style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue, 
+              foregroundColor: Colors.white, 
+            ),
+            child: Text("Yes"),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -55,7 +119,7 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
         actions: [
           TextButton(
             onPressed: () {
-              // Action du bouton Skip
+              
             },
             child: Text("Skip", style: TextStyle(color: Colors.white, fontSize: 16)),
           )
@@ -65,13 +129,13 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
       ),
       body: Stack(
         children: [
-          // Fond bleu avec le slider et texte
+          
           Container(
             padding: EdgeInsets.symmetric(vertical: 20),
             color: Colors.blue,
             child: Column(
               children: [
-                Text("How do you feel?", style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold)),
+                Text("How did you feel at this place ?", style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold)),
                 SizedBox(height: 10),
                 Text(_moodValue.emoji, style: TextStyle(fontSize: 90)),
                
@@ -79,11 +143,11 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
                 SizedBox(height: 10),
                 SliderTheme(
                   data: SliderThemeData(
-                    trackHeight: 20.0, // Épaisseur du track
-                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13.0), // Taille du curseur
-                    activeTrackColor: Colors.white, // Couleur du track
-                    inactiveTrackColor: Colors.white54, // Couleur du track quand il n'est pas actif
-                    thumbColor: Colors.white, // Couleur du curseur
+                    trackHeight: 20.0, 
+                    thumbShape: RoundSliderThumbShape(enabledThumbRadius: 13.0), 
+                    activeTrackColor: Colors.white, 
+                    inactiveTrackColor: Colors.white54, 
+                    thumbColor: Colors.white, 
                   ),
                   child: Slider(
                     value:  Mood.values.indexOf(_moodValue).toDouble(),
@@ -101,9 +165,9 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
               ],
             ),
           ),
-             // Bloc blanc avec "Describe your feelings"
+             
           Positioned(
-            top: 275, // Positionne le bloc blanc sous l'ovale
+            top: 275, 
             left: 0,
             right: 0,
             bottom: 0,
@@ -118,7 +182,7 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text("Describe your feelings", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                    Text("Describe how you felt at this place.", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                     SizedBox(height:10),
                     Wrap(
                       spacing: 10,
@@ -142,45 +206,35 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
                             
                     SizedBox(height: 20),
                             
-                    // Bloc blanc avec "Do you want to move somewhere?"
-                    Text("Do you want to move somewhere?", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                    SizedBox(height:10),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: wantsToMove ? Colors.blue : Colors.grey[300],
-                            foregroundColor: wantsToMove ? Colors.white : Colors.black,
-                            
-                          ),
-                          onPressed: () => setState(() => wantsToMove = true),
-                          child: Text("Yes"),
+                   Text("How long did you stay here?", style: TextStyle( fontSize: 18,fontWeight: FontWeight.bold)),
+                  SizedBox(height: 10),
+                  Center(
+                      child: ElevatedButton(
+                        onPressed: _pickDuration,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                         ),
-                        SizedBox(width: 10),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: !wantsToMove ? Colors.blue : Colors.grey[300],
-                            foregroundColor: !wantsToMove ? Colors.white : Colors.black,
-                          ),
-                          onPressed: () => setState(() => wantsToMove = false),
-                          child: Text("No"),
+                        child: Text(
+                          " ${_selectedDuration.inHours}h ${_selectedDuration.inMinutes % 60}m",
+                          style: TextStyle(fontSize: 16, color: Colors.white),
                         ),
-                      ],
-                    ),
+                      ),
+                    ), Spacer(),
                             
-                    Spacer(),
-                            
-                    // Bouton Submit
+                    
                     Padding(
                       padding: const EdgeInsets.only(bottom:20.0),
                       child: SizedBox(
                         width: double.infinity,
                         child: ElevatedButton(
                           onPressed: () {
+                            _showFavoriteDialog(context);
                             print("Mood: ${_moodValue.label}");
                             print("Selected Tags: $selectedTags");
-                            print("Wants to Move: $wantsToMove");
+                            print("Time: $_selectedDuration");
+                            print("Favorite: $favorites");
+
                           },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.blue,
@@ -196,7 +250,7 @@ class _MoodFormScreenState extends State<MoodFormScreen> {
             ),
           ),
         
-          // Ovale blanc au-dessus du bleu
+          
           Positioned(
             top: 275,
             left: 0,
@@ -220,9 +274,9 @@ class OvalClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     Path path = Path();
-    path.moveTo(0, size.height); // Commencer en bas à gauche
+    path.moveTo(0, size.height);
     path.quadraticBezierTo(
-      size.width / 2, -size.height / 2, size.width, size.height); // Inverser la courbure
+      size.width / 2, -size.height / 2, size.width, size.height); 
     path.lineTo(size.width, 0);
     path.lineTo(0, 0);
     path.close();
